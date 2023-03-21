411ac4d188ba294f4b78af5fdb1cf574707f32ed
Shamsuddin <taheeronline@outlook.com>
2023-03-21 22:44:07 +0530

    base URL updated

--- MVCtoConsumeAPI/Controllers/HomeController.cs
+++ MVCtoConsumeAPI/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MVCtoConsumeAPI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        string baseURL = "http://localhost:5026/api/";
+        string baseURL = "http://localhost:5026/api/ToDo";
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
